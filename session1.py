70c1f3a66b64f444c232bdabb3b2a9343cb42ef2
tli138 <tli138@ucsc.edu>
2023-10-20 20:10:17 +0800

    session 1

---
+++ session1.py
@@ -0,0 +1,13 @@
+def main():
+    full_name = "Tailin Li"
+    pronouns = "he/him"
+    favorite_movie = "8 Mile"
+    favorite_food = "Lanzhou Noodles"
+
+    print(f"My name is {full_name} and I use {pronouns} pronouns.")
+    print(f"My favorite movie is {favorite_movie}. I love the gripping storyline and Eminem's performances.")
+    print(f"I absolutely love {favorite_food}. It's so flavorful and satisfying!")
+
+
+if __name__ == "__main__":
+    main()
